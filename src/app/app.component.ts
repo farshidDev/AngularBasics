1f7aceaf4b8058c2aa4693f23ee4fb5a0d306667
farshidDev <ahmadi.farshid108@gmail.com>
2023-07-09 04:42:58 +0430

    some directives has been added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,10 @@ export class AppComponent {
   name = 'farshid ahmadi';
 
   imgURL = 'https://img.freepik.com/free-photo/blossom-floral-bouquet-decoration-colorful-beautiful-flowers-background-garden-flowers-plant-pattern-wallpapers-greeting-cards-postcards-design-wedding-invites_90220-1103.jpg?w=1060&t=st=1688336597~exp=1688337197~hmac=79dfbc6cb68631951a885bdd6add56d5a42590dc6e72f08c7164cbf343422f03';
+  images = ['https://img.freepik.com/free-photo/blossom-floral-bouquet-decoration-colorful-beautiful-flowers-background-garden-flowers-plant-pattern-wallpapers-greeting-cards-postcards-design-wedding-invites_90220-1103.jpg?w=1060&t=st=1688336597~exp=1688337197~hmac=79dfbc6cb68631951a885bdd6add56d5a42590dc6e72f08c7164cbf343422f03',
+  'https://img.freepik.com/free-photo/blossom-floral-bouquet-decoration-colorful-beautiful-flowers-background-garden-flowers-plant-pattern-wallpapers-greeting-cards-postcards-design-wedding-invites_90220-1103.jpg?w=1060&t=st=1688336597~exp=1688337197~hmac=79dfbc6cb68631951a885bdd6add56d5a42590dc6e72f08c7164cbf343422f03',
+  'https://img.freepik.com/free-photo/blossom-floral-bouquet-decoration-colorful-beautiful-flowers-background-garden-flowers-plant-pattern-wallpapers-greeting-cards-postcards-design-wedding-invites_90220-1103.jpg?w=1060&t=st=1688336597~exp=1688337197~hmac=79dfbc6cb68631951a885bdd6add56d5a42590dc6e72f08c7164cbf343422f03'];
+  
   imgAlt = 'test';
   currentDate = new Date();
   cost = 2000;
@@ -21,6 +25,7 @@ export class AppComponent {
     size: 'large'
   }
   blueClass = false;
+  fontSize = 16;
 
   getName() {
     return this.name;
